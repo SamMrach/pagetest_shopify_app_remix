5f93a6d9384d346a6d78ffbd1f0668298606bcf3
SamMrch <sammerche84@gmail.com>
2025-04-10 21:18:15 +0100

    `Update package.json and remix.config.js to enable experimental JSON modules and ES module format for server.`

--- remix.config.js
+++ remix.config.js
@@ -10,11 +10,10 @@ if (
   delete process.env.HOST;
 }
 
-/** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/.*"],
   appDirectory: "app",
-  serverModuleFormat: "cjs",
+  serverModuleFormat: "esm",
   dev: { port: process.env.HMR_SERVER_PORT || 8002 },
   future: {},
 };
