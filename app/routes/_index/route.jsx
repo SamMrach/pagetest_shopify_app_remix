b6661cf7db7e0eb80b98239829473cf42ed8e1f5
SamMrch <sammerche84@gmail.com>
2025-04-11 21:29:34 +0100

    "Removed import of login function from ../../shopify.server in route.jsx"

--- app/routes/_index/route.jsx
+++ app/routes/_index/route.jsx
@@ -1,6 +1,5 @@
 import { redirect } from "@remix-run/node";
 
-import { login } from "../../shopify.server";
 import styles from "./styles.module.css";
 
 export const loader = async ({ request }) => {
