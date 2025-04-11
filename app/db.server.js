b8033d5e3ac8e322cbdbe2e77fe08c9bc832af6b
SamMrch <sammerche84@gmail.com>
2025-04-11 13:05:04 +0100

    `Refactor db.server.js and auth routes to use PrismaClient with connection logging and limit`

--- app/db.server.js
+++ app/db.server.js
@@ -1,11 +1,18 @@
+// utils/db.server.ts or lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-if (process.env.NODE_ENV !== "production") {
-  if (!global.prismaGlobal) {
-    global.prismaGlobal = new PrismaClient();
-  }
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var __prisma: PrismaClient | undefined;
 }
 
-const prisma = global.prismaGlobal ?? new PrismaClient();
+const prisma =
+  global.__prisma ||
+  new PrismaClient({
+    log: ["query", "info", "warn", "error"],
+  });
+
+if (process.env.NODE_ENV !== "production") global.__prisma = prisma;
 
 export default prisma;
